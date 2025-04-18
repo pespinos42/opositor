199a88d44474b809a0e54c39ae39044da0cbf1a5
pespinos42 <pabloespinosagomez@protonmail.com>
2025-04-18 17:36:49 +0200

    comprobacion fichero por numero de lineas OK

--- src/main.c
+++ src/main.c
@@ -9,18 +9,22 @@ int main(int argc, char **argv)
 {
     //atexit(ft_leaks);
     int     fd;
-    char    respuesta;
-    char    *respuesta_fichero;
+    //char    respuesta;
+    //char    *respuesta_fichero;
     char    *cadena_imprimir;
-    int     total_preguntas;
-    int     total_aciertos;
-    int     total_errores;
-    int	    linea_error;
+    //int     total_preguntas;
+    //int     total_aciertos;
+    //int     total_errores;
+    //int	    linea_error;
+    char    *comprobacion_total_preguntas;
+    int     total_lineas;
 
-    total_preguntas = 0;
-    total_aciertos = 0;
-    total_errores = 0;
-    linea_error = 1;
+    //total_preguntas = 0;
+    //total_aciertos = 0;
+    //total_errores = 0;
+    //linea_error = 1;
+    comprobacion_total_preguntas = 0;
+    total_lineas = 0;
 
     if (argc != 2)
     {
@@ -34,7 +38,38 @@ int main(int argc, char **argv)
         printf("NO SE ENCUENTRA EL FICHERO\n");
         exit(1);
     }
+
+    //PARTE NUEVA DE COMPROBACION POR NUMERO DE LINEAS
+
+    comprobacion_total_preguntas = ft_get_next_line(fd);
     cadena_imprimir = ft_get_next_line(fd);
+    total_lineas++;
+    while (cadena_imprimir)
+    {
+	    cadena_imprimir = ft_get_next_line(fd);
+	    if (cadena_imprimir)
+		    total_lineas++;
+    }
+
+    if (total_lineas / 6 == atoi(comprobacion_total_preguntas))
+    {
+	    printf("TOTAL PREGUNTAS = %i\n", atoi(comprobacion_total_preguntas));
+	    printf("TOTAL LINEAS = %i\n", total_lineas);
+	    printf("TOTAL LINEAS / 6 = %i\n", total_lineas/6);
+	    printf("FICHERO CORRECTO\n");
+    }
+    else
+    {
+        printf("TOTAL PREGUNTAS = %i\n", atoi(comprobacion_total_preguntas));
+	    printf("TOTAL LINEAS = %i\n", total_lineas);
+	    printf("TOTAL LINEAS / 6 = %i\n", total_lineas/6);
+	    printf("FICHERO CORRUPTO\n");
+	    exit(1);
+    }
+
+    //FIN DE PARTE NUEVA DE COMPROBACION POR NUMERO DE LINEAS
+
+/*    cadena_imprimir = ft_get_next_line(fd);
 
     printf("COMPROBANDO INTEGRIDAD DEL FICHERO\n");
     while (cadena_imprimir && cadena_imprimir[0] != '\n')
@@ -112,5 +147,7 @@ int main(int argc, char **argv)
         printf("\033[1;37;42mTODO PERFE!\033[0m\n");        
     }
     printf("\n");
+
+    */
     return (0);
 }
